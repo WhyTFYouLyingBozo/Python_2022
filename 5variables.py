619c3a8a753ab89fac8c2ed8f88ac34f4b040292
Jaiy bozzo <jaipra99@kingshigh.school.nz>
2022-03-03 09:55:26 +1300

    daddy drake sii

---
+++ 5variables.py
@@ -0,0 +1,8 @@
+#excercise 5.4
+player_lives = 3
+user_login = "jobolo420"
+user_money = 40
+user_phone_number = "021222222"
+user_name = "jaiy"
+user_age = 15
+
---
+++ var.py
@@ -0,0 +1,40 @@
+"""
+print("hello jaiy")
+#excercise 5.4 1-11
+player_lives = 3
+user_login = "jobolo420"
+user_money = 40
+user_phone_number = "021222222"
+user_name = "jaiy"
+user_age = 15
+user_year = 2022
+user_surname = "pratap"
+print(player_lives)
+print(user_login)
+print(user_money)
+print(user_phone_number)
+print(user_name)
+print(user_age)
+print(user_year)
+print(user_surname)
+#excercise 5.4 12-21
+user_school = "Kingshigh"
+user_fav_food = "Pizza"
+user_petname = "Tigger"
+user_blockchoc = 4 
+player_score = 1
+players_highest_score = 2
+user_laps_completed = 0.5
+user_ammunition_remaining = 2
+user_gear = "4th gear"
+user_speed = "30mh"
+"""
+#Excercise 5.6
+user_money = 200
+hat = 20
+top = 30
+pants = 15
+belt = 60
+shoes = 40
+print(user_money)
+print(user_money - hat)
