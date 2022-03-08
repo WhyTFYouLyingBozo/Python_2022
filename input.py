6a1fdb5c05a0c57d0fcc73c6e1578fe6aef49cc6
Jaiy bozzo <jaipra99@kingshigh.school.nz>
2022-03-08 13:22:29 +1300

    Update input.py
    Another day, another daddy drake committed to main siii crsiete donda esta, daddy drake god rahul hbd

--- input.py
+++ input.py
@@ -1,4 +1,5 @@
 #6.3 Excercises input
+""" #1-11
 enter_key = input(" Please push any key then press enter")
 print(enter_key)
 enter_name = input("Please enter your name")
@@ -29,3 +30,28 @@ print(f"I never really got around to watching {user_movie}, looks pretty trash")
 print(f"{user_book} isn't that good, how about getting a life.")
 print(f"Oh that's a pretty cool {user_noun}, I don't really care tho")
 print(f"Oh, so you are gonna go {user_verb}, lameee")
+
+#12-17
+user_age = int(input("How old are you?"))
+print(f"In ten years you will be {user_age + 10} Years old")
+print(f"You were born in the year{2021 - user_age}")
+user_apples = int(input("How many apples do you have?"))
+user_friends = int(input("How many friends you have?"))
+print(f"Each friend can have {user_apples % user_friends} apples")
+
+#18 - 20
+user_pizza = int(input("How many Pizza you want"))
+user_peeps = int(input("How many peeps you feedin?"))
+print( f"Each person will get ({user_pizza * 8 // user_peeps }) slices ")
+
+#21 - 24
+user_dollars = int(input("How many dollars you have?"))
+user_TV = int(input("How much does TV cost?"))
+print(f"If you buy the TV you will have {user_dollars - user_TV} dollars left")
+print(f"There is a 20% sale on the TV so it will cost {user_TV *.8} dollars")
+
+#25 - 27
+user_bitcoins = int(input("How many bitcoins you own?"))
+current_value = 55717.81
+print(f"Your current bitcoin value ${user_bitcoins * current_value } NZD ")
+"""
