887b4b0ebd17e7323b47c0f813924d8f54feae35
Jaiy bozzo <jaipra99@kingshigh.school.nz>
2022-03-14 10:52:13 +1300

    Update conditional.py
    Daddy drake baby get back into it by myself ye happy days conditional.

--- conditional.py
+++ conditional.py
@@ -1,4 +1,5 @@
 #7.5 excercises
+"""
 #Question 1
 ice_creams = int(input("How many ice creams do you need"))
 if ice_creams <= 20:
@@ -27,3 +28,29 @@ elif user_favmovie != "Lord of the Rings":
 user_ask = input("Have you hear of darth Plagueis?")
 if user_ask == "no":
 	print("Darth Plagueis is a fictional character in the Star Wars franchise. A Sith Lord with the ability to prevent death and create life, Plagueis is the mentor of Palpatine (Darth Sidious). ")
+elif user_ask:
+	print("You must be a fan")
+#Question 6 
+who_directed = input("Who directed 'Passion of Christ' ?")
+if who_directed == "Mel Gibson" "mel gibson" "Mel gibson":
+	print("Correct")
+elif who_directed:
+	print("Mel Gibson did")
+	"""
+#Question 7
+score = 0
+Question_1 = int(input("What is 9/3 =?"))
+if Question_1 == 3:
+	print("correct")
+	1 + score
+else:
+	print("Incorrect, the answer was 3") 
+
+Question_2 = int(input("How old is Arnold schwarzenegger?"))
+if Question_2 == 73:
+	print("correct")
+else:
+	print("Incorrect, he is 73 years old")
+
+
+
