2f92ece6e3a74d36bbe159d81832dc4fea2074e0
Jaiy bozzo <jaipra99@kingshigh.school.nz>
2022-03-22 09:56:56 +1300

    meh
    Daddy drake fap with emanny but no finny cause he isoiso but emanny back from covid, ye gittyhubby hehe yolo ur dad

--- dom.py
+++ dom.py
@@ -17,18 +17,43 @@ def comb_hair():
 	spaceship = Room("""
 		The bridge if the spaceship is shiny and white, with thousands of small, red, blinking lights.
 		""")
-current_room = space
+	quarters = Room("""
+		This is the quarters, most of the contolling and organising is done here 
+		""")
+	mass_hall = Room("""
+		This is the mass hall, mostly where everyone is and a great place to chill
+		""")
+	hallway = Room("""
+		This is the hallway. It connects most of the rooms together
+		""")
+	cargo = Room("""
+		This is Cargo, this is where most of the luggage,food and other things are kept
+		""")
+	docking = Room("""
+		This is Docking, where we recieve and have stuff arrive
+		""")
+	bridge = Room("""
+		This is the bridge, it connects the hallway to the escape pods.
+		""")
+	escape_pods = Room("""
+		These are the escape pods, in emergencies, these are very crucial.
+		""")
+	current_room = space
 
 @when("enter airlock")
 @when("enter spaceship")
 @when("enter ship")
 def enter_spaceship():
-def enter_spaceship():
 	global currrent_room
 #check if action can be done
-if current_room is not space:
-	say("there is no airlock here")
-	return
+	if current_room is not space:
+		say("there is no airlock here")
+		return
+	else:
+		current_room = enter_spaceship
+		print("""You leave yourself into the spaceship and slam your hand on the button to close the door.
+			""")
+		print(current_room)
 def main():
 	start()
 
