18c8e15e431f394a451760a6d3632b617088c555
Jaiy bozzo <jaipra99@kingshigh.school.nz>
2022-03-23 10:58:47 +1300

    Update dom.py
    drake ye

--- dom.py
+++ dom.py
@@ -1,44 +1,53 @@
 from adventurelib import *
-@when("brush teeth")
-@when("brush")
-@when("clean teeth")
-def brush_teeth():
-	print("You brush your teeth")
 
-@when("comb hair")
-@when("comb")
-def comb_hair():
-	say("""
-		You brush your long flowing locks with the gold hairbrush that you have selected from the red basket 
-		""")
-	space = Room("""
-	You are drifting in space. It feels very cold. A slate-blue spaceship sits completely silently to your left,it's airlock open and waiting 
-	""" )
-	spaceship = Room("""
-		The bridge if the spaceship is shiny and white, with thousands of small, red, blinking lights.
-		""")
-	quarters = Room("""
-		This is the quarters, most of the contolling and organising is done here 
-		""")
-	mass_hall = Room("""
-		This is the mass hall, mostly where everyone is and a great place to chill
-		""")
-	hallway = Room("""
-		This is the hallway. It connects most of the rooms together
-		""")
-	cargo = Room("""
-		This is Cargo, this is where most of the luggage,food and other things are kept
-		""")
-	docking = Room("""
-		This is Docking, where we recieve and have stuff arrive
-		""")
-	bridge = Room("""
-		This is the bridge, it connects the hallway to the escape pods.
-		""")
-	escape_pods = Room("""
-		These are the escape pods, in emergencies, these are very crucial.
-		""")
-	current_room = space
+space = Room("""
+You are drifting in space. It feels very cold. A slate-blue spaceship sits completely silently to your left,it's airlock open and waiting 
+""" )
+spaceship = Room("""
+	The bridge if the spaceship is shiny and white, with thousands of small, red, blinking lights.
+	""")
+quarters = Room("""
+	This is the quarters, most of the contolling and organising is done here 
+	""")
+mass_hall = Room("""
+	This is the mass hall, mostly where everyone is and a great place to chill
+	""")
+hallway = Room("""
+	This is the hallway. It connects most of the rooms together
+	""")
+cargo = Room("""
+	This is Cargo, this is where most of the luggage,food and other things are kept
+	""")
+docking = Room("""
+	This is Docking, where we recieve and have stuff arrive
+	""")
+bridge = Room("""
+	This is the bridge, it connects the hallway to the escape pods.
+	""")
+escape_pods = Room("""
+	These are the escape pods, in emergencies, these are very crucial.
+	""")
+current_room = space
+	
+spaceship.east = hallway
+spaceship.south = quarters
+hallway.east = bridge
+hallway.north = cargo
+hallway.south = mass_hall
+quarters.east = mass_hall
+cargo.east = docking
+bridge.south = escape_pods
+
+@when ("go DIRECTION")
+def travel(direction):
+	global currrent_room
+	if direction in current_room.exits():
+		currrent_room = current_room.exit(direction)
+		print(f"you go {direction}.")
+		print(current_room)
+
+
+
 
 @when("enter airlock")
 @when("enter spaceship")
@@ -55,6 +64,7 @@ def enter_spaceship():
 			""")
 		print(current_room)
 def main():
+	print(current_room)
 	start()
 
 if __name__ == '__main__':
