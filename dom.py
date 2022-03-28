47d6d472106511ae2cec173347c2f608cd9bf22c
Jaiy bozzo <jaipra99@kingshigh.school.nz>
2022-03-28 14:56:54 +1300

    daddy drake.py
    finny annoying but now im one handed af so ye shii, its gonna be hard but seman will halp oh yeah sewey

--- dom.py
+++ dom.py
@@ -1,5 +1,5 @@
 from adventurelib import *
-
+Room.items = Bag()
 space = Room("""
 You are drifting in space. It feels very cold. A slate-blue spaceship sits completely silently to your left,it's airlock open and waiting 
 """ )
@@ -9,21 +9,25 @@ spaceship = Room("""
 quarters = Room("""
 	This is the quarters, most of the contolling and organising is done here 
 	""")
+goggles.items.add(goggles)
 mass_hall = Room("""
 	This is the mass hall, mostly where everyone is and a great place to chill
 	""")
+	mass_hall.items.add(red_keycard)
 hallway = Room("""
 	This is the hallway. It connects most of the rooms together
 	""")
 cargo = Room("""
 	This is Cargo, this is where most of the luggage,food and other things are kept
 	""")
+cargo.items.add(knife)
 docking = Room("""
 	This is Docking, where we recieve and have stuff arrive
 	""")
 bridge = Room("""
 	This is the bridge, it connects the hallway to the escape pods.
 	""")
+USB.items.add(USB)
 escape_pods = Room("""
 	These are the escape pods, in emergencies, these are very crucial.
 	""")
@@ -46,13 +50,37 @@ def travel(direction):
 		print(f"you go {direction}.")
 		print(current_room)
 
+@when("Look")
+def look():
+	print(current_room)
+	print(f"There are exits to the {current_room.exits()}.")
+	if len(current_room.items) > 0: #if there are some items in the room
+		print("You also see:")
+		for item in current_room.items:
+			print(item)#print out each item
 
+Item.description - "" #this adds a blank description to each item
 
+knife = Item("a dirty knife","knife")
+knife.description = "the knife has a dull sheen to it but it looks rather sharp."
+
+red_keycard = Item("a red keycard","keycard", "red card",)
+red_keycard.description = "It's a red keycard. It probably opens a door or a locker."
+
+goggles = Item("goggles", "glasses")
+goggles.description = "Round circular goggles, help to see certain things the naked eye can't"
+
+USB = Item("USB", "usb", "Usb")
+USB.description = "Regular USB, but it stores some interesting files..."
+
+#define variables
+current_room = space
+inventory = bag()
 
 @when("enter airlock")
 @when("enter spaceship")
 @when("enter ship")
-def enter_spaceship():
+def enter_airlock
 	global currrent_room
 #check if action can be done
 	if current_room is not space:
