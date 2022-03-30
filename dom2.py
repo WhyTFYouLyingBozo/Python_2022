1f14ae30deafc4699b0816ca666e9e74dfcc69dd
Jaiy bozzo <jaipra99@kingshigh.school.nz>
2022-03-31 10:59:49 +1300

    Update dom2.py
    Daddy drake with emanny ong abby newell nice af, pattyy tyrell was call last night oh ye

--- dom2.py
+++ dom2.py
@@ -1,2 +1,21 @@
 from adventurelib import *
+#Room.items = Bag()
 
+print(f"You have secured the treasure of King Hotemprah.\n Now  you must make your way through the pyramid through the Thick and thin and reach the bottom,\n one wrong however and you could be stuck for an eternity.\n Good Luck!, you will need it." )
+
+kings_tomb = Room(""" This is King Hotemprah's room, filled with treasures no eye has ever seen,\n
+ gold as bright as the sun. Don't be greedy.
+	""")
+queens_room = Room(""" This is the Queens room,\n
+	you will find treasures as well but have a look around, there might just be something interesting.
+
+
+	""")
+
+
+def main():
+	#print(current_room)
+	start()
+
+if __name__ == '__main__':
+	main()
